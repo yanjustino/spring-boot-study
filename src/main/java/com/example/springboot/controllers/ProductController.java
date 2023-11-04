283721f4dbd6f392acc56b51c4614d49ac55a23f
yanjustino <yanlimaj@gmail.com>
2023-11-04 09:37:26 -0300

    first commit

---
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,41 @@
+package com.example.springboot.controllers;
+
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+public class ProductController {
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/{id}/product")
+    public ResponseEntity<Optional<ProductModel>> getProductById(@PathVariable UUID id){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findById(id));
+    }
+}
